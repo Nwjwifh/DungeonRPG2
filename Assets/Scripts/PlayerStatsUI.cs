eeb44d15c8cfd52dd92b4e51b641ec3445540992
Nwjwifh <111439484+Nwjwifh@users.noreply.github.com>
2023-09-19 14:53:03 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerStatsUI.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerStatsUI : MonoBehaviour
+{
+    public TMP_Text playerNameText;
+    public TMP_Text playerLevelText;
+    public TMP_Text playerGoldText;
+    public TMP_Text playerAttackText;
+    public TMP_Text playerDefenseText;
+    public TMP_Text playerMaxHPText;
+    public TMP_Text playerMaxMPText;
+    public TMP_Text playerMaxSPText;
+    public TMP_Text playerLuckText;
+
+    public PlayerSO playerData;
+
+
+    private void Start()
+    {
+        UpdatePlayerStatsUI();
+    }
+
+    void UpdatePlayerStatsUI()
+    {
+        // TMP Text UI 요소에 플레이어 정보 표시
+        playerNameText.text = playerData.playerName;
+        playerLevelText.text = "LV. " + playerData.level.ToString();
+        playerGoldText.text = playerData.gold.ToString();
+        playerAttackText.text = "ATK: " + playerData.atk.ToString();
+        playerDefenseText.text = "DEF: " + playerData.def.ToString();
+        playerMaxHPText.text = "HP: " + playerData.maxHP.ToString();
+        playerMaxMPText.text = "MP: " + playerData.maxMP.ToString();
+        playerMaxSPText.text = "SP: " + playerData.maxSP.ToString();
+        playerLuckText.text = "LUK: " + playerData.luk.ToString();
+    }
+}
